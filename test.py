90a352d7066dcbe2d207b9dc87d7a0126a69250e
jr011 <60546139+jr011@users.noreply.github.com>
2022-09-22 20:20:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,28 @@
-a = [1,2,3,4,5,6]
-b = a[6]
-print(b)
-def adder(n):
-     num = 0
-     while num < n:
-         yield num
-         num += 1
-     return num  #Noncompliant
+from torch import nn
+import torch
+import torch.nn.functional as F
 
-adder(b)
+# rnn = nn.LSTM(10, 20, 2)
+# input = torch.randn(5, 3, 10)
+# h0 = torch.randn(2, 3, 10)
+# c0 = torch.randn(2, 3, 10)
+# output, (hn, cn) = rnn(input, (h0, c0))  # output(5,3,20)
+# print(output.shape)
+
+
+class DropoutFC(nn.Module):
+    def __init__(self):
+        super(DropoutFC, self).__init__()
+        self.fc = nn.Linear(100,20)
+
+    def forward(self, input):
+        out = self.fc(input)
+        out = F.dropout(out, p=0.5)
+        return out
+
+device = 'cuda'
+
+Net = DropoutFC().to(device)
+Net.train()
+input = torch.randn(100,device='cuda')
+out = Net(input)
